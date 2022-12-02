0c262251ffcb7a705a41d31414d4eecea8886b5f
ncilliez <ncilliez@gmail.com>
2022-12-02 12:26:27 +0100

    carousel timer

--- js/script.js
+++ js/script.js
@@ -3,9 +3,12 @@
 
         margin: 0,
         nav: true,
+        loop: true,
         navText: ["<i class='icon fa-sharp fa-solid fa-circle-chevron-left'></i>", "<i class='icon fa-sharp fa-solid fa-circle-chevron-right'></i>"],
         responsiveRefreshRate: 0,
         autoWidth: true,
+        autoplay: true,
+        autoplayTimeout: 4000,
  
 
     });
